94a17146c2967c3d07f7cd6143961958bdb8b2c8
Stewart Stewart <stewinsalot@gmail.com>
2015-01-13 08:06:38 -0700

    README and styling to change size of selected cards

---
+++ README.md
@@ -0,0 +1,11 @@
+## Game Rules:
+
+The deck has 63 distict cards. At any given time, the top 7 cards are
+displayed. The goal is to collect legal subsets (prosets) of the
+upcards until the deck is empty. Every time a proset is taken, the
+upcards are re-stocked from the deck.
+
+A legal set is one such that there are an even number of every
+position of dot. Another way to think of it is that each card
+represents a number in binary. A proset is one that reduces to zero
+with XOR.
--- src/main/scala/Proset.scala
+++ src/main/scala/Proset.scala
@@ -1,40 +1,40 @@
 import scala.scalajs.js.JSApp
 import scala.scalajs.js.annotation.JSExport
-import org.scalajs.jquery.jQuery
+import org.scalajs.jquery.{jQuery => $}
 
 object Proset extends JSApp {
   val DOTS = 1 to 6
   var deck = Deck()
 
   def main(): Unit = {
-    jQuery(setupUI _)
-    jQuery(deal _)
+    $(setupUI _)
+    $(deal _)
   }
 
   def setupUI(): Unit = {
-    jQuery("body").append(View.gameTable.render)
-    jQuery("#game-table")
+    $("body").append(View.gameTable.render)
+    $("#game-table")
       .dblclick(submit _)
     (1 to 7) foreach { n =>
-      jQuery(s"#slot-$n")
+      $(s"#slot-$n")
         .click(toggle(n) _)
     }
   }
 
   def tearDownUI(): Unit = {
-    jQuery("#game-table").remove()
+    $("#game-table").remove()
   }
 
   def insert(card: Int): Unit =
-    jQuery("#game-table .slot:empty:first").append(View.card(card).render)
+    $("#game-table .slot:empty:first").append(View.card(card).render)
 
   def deal(): Unit = deck.upcards
-    .filter(n => jQuery(s"#card-$n").length == 0)
+    .filter(n => $(s"#card-$n").length == 0)
     .map(insert _)
 
   @JSExport
   def toggle(slot: Int)(): Unit = {
-    if (jQuery(s"#slot-$slot .card-chosen").length == 0) {
+    if ($(s"#slot-$slot .card-chosen").length == 0) {
       select(slot)
     } else {
       deselect(slot)
@@ -42,24 +42,24 @@ object Proset extends JSApp {
   }
 
   def select(slot: Int): Unit = {
-    jQuery(s"#game-table #slot-$slot .card")
+    $(s"#game-table #slot-$slot .card")
       .attr("class", "card card-chosen")
   }
 
   def deselect(slot: Int): Unit =
-    jQuery(s"#game-table #slot-$slot .card")
+    $(s"#game-table #slot-$slot .card")
       .attr("class", "card")
 
   @JSExport
   def submit(): Unit = {
     val chosen: Set[Int] = (1 to 7)
-      .filter(n => jQuery(s"#slot-$n .card-chosen").length > 0)
-      .map(n => jQuery(s"#slot-$n .card-chosen").attr("id").split('-').last.toInt)
+      .filter(n => $(s"#slot-$n .card-chosen").length > 0)
+      .map(n => $(s"#slot-$n .card-chosen").attr("id").split('-').last.toInt)
       .toSet
     val newDeck = deck.remove(chosen)
     if (newDeck.upcards != deck.upcards) {
       this.deck = newDeck
-      jQuery(".card-chosen").remove()
+      $(".card-chosen").remove()
       deal()
     } else {
       (1 to 7) foreach deselect
